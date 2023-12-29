6c61299a8b1348bb4e78b33e40fadad476410100
Sugianto <sugianto228@gmail.com>
2023-12-29 22:47:46 +0800

    fix github page routing

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ export default function App() {
   return (
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/game" element={<Game />} />
+          <Route path="" element={<Home />} />
+          <Route path="game" element={<Game />} />
         </Routes>
       </div>
   );
