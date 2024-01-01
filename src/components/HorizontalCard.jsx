671cbee0c7ac3b2a949ec0df05178bd2f6d72e4f
Sugianto <sugianto228@gmail.com>
2024-01-02 00:53:07 +0800

    clean up personal projects files

--- src/components/HorizontalCard.jsx
+++ src/components/HorizontalCard.jsx
@@ -1,6 +1,4 @@
 
-import { motion } from 'framer-motion';
-
 import { PingLatest } from '.';
 
 const HorizontalCard = ({ url, alt, title, href, index }) => (
--- src/home_page/PersonalProject.jsx
+++ src/home_page/PersonalProject.jsx
@@ -1,11 +1,11 @@
 
-import { useState, useRef } from 'react';
+import { useRef } from 'react';
 import { motion, useScroll, useTransform } from 'framer-motion';
 
 import { fadeIn, staggerContainer } from '../styles/motion';
 import styles from '../styles/styles';
-import { ProjectCard, HorizontalCard } from '../components';
-import { schoolProjects, personalProjects } from '../constants';
+import { HorizontalCard } from '../components';
+import { personalProjects } from '../constants';
 
 const PersonalProject = () => {
     // Personal projects state
@@ -49,36 +49,4 @@ const PersonalProject = () => {
     );
 };
 
-export default PersonalProject; 
-
-
-// {/* Personal projects */}
-            // <motion.div 
-            //     className={`${styles.innerWidth} pt-5 mx-auto flex flex-col`}
-            //     variants={staggerContainer}
-            //     initial="hidden"
-            //     whileInView="show"
-            //     viewport={{ once: false, amount: 0.25 }}
-            // >
-            //     <motion.h1 
-            //         className={`${styles.mainTitle}`}
-            //         variants={fadeIn("up", "tween", 0.2, 1)}
-            //     >
-            //         My Projects
-            //     </motion.h1>
-            //     <motion.h1 
-            //         className={`${styles.subTitle}`}
-            //         variants={fadeIn("up", "tween", 0.4, 1)}
-            //     >
-            //         Explore my passion.
-            //     </motion.h1>
-            // </motion.div>
-            // <section ref={targetRef} className={`${styles.innerWidth} mx-auto relative h-[300vh] bg-neutral-900`}>
-            //     <div className="sticky top-0 flex items-center  overflow-hidden">
-            //         <motion.div style={{ x }} className="flex gap-4">
-            //         {personalProjects.map((project, index) => {
-            //             return <HorizontalCard {...project} index={index} />;
-            //         })}
-            //         </motion.div>
-            //     </div>
-            // </section>
+export default PersonalProject;
