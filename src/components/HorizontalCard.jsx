b7b488ec90a69f8cbc6313dae2eb8cc510f61045
Sugianto <sugianto228@gmail.com>
2024-01-12 21:54:48 +0800

    renponsive sizing cards

--- src/components/HorizontalCard.jsx
+++ src/components/HorizontalCard.jsx
@@ -2,7 +2,7 @@
 import { PingLatest } from '.';
 
 const HorizontalCard = ({ url, alt, title, href, index }) => (
-    <div className="relative h-[50vh] w-[700px] overflow-hidden cursor-pointer">
+    <div className="relative h-[50vh] w-[280px] md:w-[490px] lg:w-[700px] overflow-hidden cursor-pointer">
         { index === 0 && <PingLatest />}
         <a href={href} target="_blank" rel="noreferrer" draggable="false">
             <img
@@ -12,7 +12,7 @@ const HorizontalCard = ({ url, alt, title, href, index }) => (
                 className="absolute w-full h-full object-cover rounded-[24px]"
             />
             <div className="absolute w-full bottom-0">
-                <p className="text-white p-8 text-4xl font-black">
+                <p className="text-white p-8 lg:text-[50px] md:text-[40px] text-[30px] font-black">
                     {title}
                 </p>
             </div>
