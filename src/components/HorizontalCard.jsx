e1bcfa839af8315b5349000fe87e261bc78ac7cc
Sugianto <sugianto228@gmail.com>
2024-01-14 22:09:20 +0800

    fix ui

--- src/components/HorizontalCard.jsx
+++ src/components/HorizontalCard.jsx
@@ -2,7 +2,7 @@
 import { PingLatest } from '.';
 
 const HorizontalCard = ({ url, alt, title, href, index }) => (
-    <div className="relative h-[50vh] w-[150px] min-[375px]:w-[200px] min-[425px]:w-[230px] min-[570px]:w-[330px] md:w-[400px] lg:w-[570px] min-[1420px]:w-[830px] overflow-hidden cursor-pointer">
+    <div className="relative h-[50vh] w-[150px] min-[375px]:w-[200px] min-[425px]:w-[230px] min-[570px]:w-[330px] md:w-[400px] lg:w-[570px] min-[1420px]:w-[830px] cursor-pointer">
         { index === 0 && <PingLatest />}
         <a href={href} target="_blank" rel="noreferrer" draggable="false">
             <img
